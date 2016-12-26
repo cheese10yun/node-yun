d08ff8193903843445d1d923f072479eaa56954c
yun <cheese10yun@gmail.com>
2016-12-26 12:06:28 +0900

    passport login 	index.js

--- app.js
+++ app.js
@@ -5,14 +5,26 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+var passport = require('passport'); //passport module add
+var cookieSession = require('cookie-session');
+
+/*router*/
 var index = require('./routes/index');
 var users = require('./routes/users');
+/*router*/
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+
+/*필수 블로그 소개글에 추가시킬*/
+app.use(cookieSession({
+  keys: ['node_yun']
+}));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,6 +33,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use('/', index);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+/**
+ * */
+
+/*로그인 성공시 사용자 정보를 Session에 저장한다*/
+passport.serializeUser(function (user, done) {
+  done(null, user)
 });
 
+/*인증 후, 페이지 접근시 마다 사용자 정보를 Session에서 읽어옴.*/
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+/*블로그 소개시 이함수 소개하자*/
+var isAuthenticated = function (req, res, next) {
+  if (req.isAuthenticated())
+    return next();
+  res.redirect('/login');
+};
+
+passport.use(new LocalStrategy({
+  usernameField: 'user_id',
+  passwordField: 'password',
+  passReqToCallback: true //인증을 수행하는 인증 함수로 HTTP request를 그대로  전달할지 여부를 결정한다
+}, function (req, user_id, password, done) {
+  if (false) {
+    return done(null, false);
+  } else {
+    var user = {
+      user: user_id
+  };
+    return done(null, user)
+  }
+}));
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', {title: 'Express'});
+});
+
+
 module.exports = router;
+
+
